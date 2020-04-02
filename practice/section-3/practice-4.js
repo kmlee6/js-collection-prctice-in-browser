eaaf7ad005c9c9db561795aa57172ff9f391733d
kmlee6 <32027946+kmlee6@users.noreply.github.com>
2020-04-03 00:30:28 +0800

    [Task3] count common

--- practice/section-3/practice-4.js
+++ practice/section-3/practice-4.js
@@ -1,5 +1,7 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return 'Implement the practice require, and begin changing code in this row';
+	var counter = 0;
+	var tmp = collectionA.map(x => {if(x.match(/(?<index>.*)-(?<count>[0-9]*)/)==null){return [x];}else{let {index, count} = x.match(/(?<index>.*)-(?<count>[0-9]*)/).groups; return [...Array(parseInt(count))].map((_, i) => index)}}).reduce((result, part) => result.concat(part));
+  return [...new Set(tmp)].map(x => [x, tmp.filter(y => y === x).length]).map(subArr => {return {key: subArr[0], count: subArr[1]}}).map(ele => {if(objectB["value"].includes(ele["key"])){ele["count"]-=((counter%3)+1); counter++} ;return ele});
 }
