e3d42efc5a3837558f5c9785708e4dc287afc539
kmlee6 <32027946+kmlee6@users.noreply.github.com>
2020-04-02 23:49:56 +0800

    [Task3] counting problem

--- practice/section-3/practice-3.js
+++ practice/section-3/practice-3.js
@@ -1,5 +1,6 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return 'Implement the practice require, and begin changing code in this row';
+	var counter = 0;
+  return [...new Set(collectionA)].map(x => [x, collectionA.filter(y => y === x).length]).map(subArr => {return {key: subArr[0], count: subArr[1]}}).map(ele => {if(objectB["value"].includes(ele["key"])){ele["count"]-=((counter%3)+1); counter++} ;return ele});
 }
